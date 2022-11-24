fd44ca8048997dfa03f61a3192b3614329574d4d
anhphuongtran01ql <anhphuongtran01ql@gmail.com>
2022-11-25 01:01:48 +0700

    Add auth and middleware

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,76 @@
+const db = require("../models/index");
+const config = require("../config/auth.config");
+const cache = require('../utils/cache');
+const ROLES = db.Role;
+const User = db.User;
+const Op = db.Sequelize.Op;
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+exports.signup = async (req, res) => {
+    // Save User to Database
+    await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8)
+    }).then(user => {
+        res.send(user);
+    })
+        .catch(err => {
+            res.status(500).send({message: err.message});
+        });
+};
+
+exports.login = async (req, res) => {
+    await User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+        .then(async user => {
+            if (!user) {
+                return res.status(404).send({message: "User Not found."});
+            }
+            var passwordIsValid = bcrypt.compareSync(
+                req.body.password,
+                user.password
+            );
+
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    accessToken: null,
+                    message: "Invalid Password!"
+                });
+            }
+
+            var token = await jwt.sign({id: user.id}, config.secret, {
+                expiresIn: 86400 // 24 hours
+            });
+
+            return res.json({
+                access_token: token,
+                token_type: 'Bearer',
+                expires_in: 86400
+            });
+        })
+        .catch(err => {
+            res.status(500).send({message: err.message});
+        });
+};
+
+exports.getUser = async (req, res) => {
+    const user = await User.findByPk(req.user.id);
+    return res.json(user);
+}
+
+exports.logout = async (req, res) => {
+    const token = req.token;
+    const now = new Date();
+    const expire = new Date(req.user.exp);
+    const milliseconds = now.getTime() - expire.getTime();
+    /* ----------------------------- BlackList Token ---------------------------- */
+    await cache.set(token, token, milliseconds);
+
+    return res.json({message: 'Logged out successfully'});
+}
--- src/route/web.js
+++ src/route/web.js
@@ -1,14 +1,20 @@
 import express from "express";
 import userController from "../controllers/userController";
+import {verifySignUp} from "../middleware";
+import authController from "../controllers/auth.controller";
+const { authJwt } = require("../middleware");
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
   router.get("/users", userController.getUsers);
-  router.get("/user", userController.getUser);
   router.post("/create-user", userController.createUser);
   router.put("/edit-user", userController.editUser);
   router.get("/delete-user", userController.deleteUser);
+  router.post("/auth/signup",[verifySignUp.checkDuplicateUsernameOrEmail], authController.signup);
+  router.post("/auth/login", authController.login);
+  router.get("/user", [authJwt.verifyToken], authController.getUser);
+  router.get('/logout', [authJwt.verifyToken], authController.logout);
 
   return app.use("/", router);
 };
